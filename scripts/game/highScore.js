098f3f80330cb418ad6a13c95a8460c611814fc3
tcortes55 <35512873+tcortes55@users.noreply.github.com>
2020-06-30 22:34:01 -0300

    Reading high scores from Firebase

---
+++ scripts/game/highScore.js
@@ -0,0 +1,12 @@
+class HighScore {
+    constructor(name, scoreDay, scoreHour, scoreMinute, totalPretzels, totalCrosswords, totalDaysAllPretzelsPicked, totalFirstAidOccurrences) {
+        this.name = name;
+        this.scoreDay = scoreDay;
+        this.scoreHour = scoreHour;
+        this.scoreMinute = scoreMinute;
+        this.totalPretzels = totalPretzels;
+        this.totalCrosswords = totalCrosswords;
+        this.totalDaysAllPretzelsPicked = totalDaysAllPretzelsPicked;
+        this.totalFirstAidOccurrences = totalFirstAidOccurrences;
+    }
+}
